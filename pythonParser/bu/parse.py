31deacb3ba47ea5d85411cc9696d01883d2c51c7
BuFuuu <lion@familie-hellstern.de>
2016-04-06 23:52:36 +0200

    Feature Images: -added function for downloading a img -added func for converting --cause its anoying to handle svgs in latex, I convert them to pdf with --inkscape --added func for getting the correct img-link. I download the img form --the: de.wikibooks.org/wiki/Datei: --it will be saved in a "Bilder" dir
    Todo:
    -replace the figure tag with latex code

--- pythonParser/bu/parse.py
+++ pythonParser/bu/parse.py
@@ -39,44 +39,42 @@ def downloadPictureFromWikifile(fileUrl, fileName):
     urllib.request.urlretrieve(picUrl, fileName)
 
 #Need inkscape installed!
-def convertImgSvgToPdf(imgPath):
-    outName = imgPath[:-4] + ".pdf"
-    os.system("inkscape -f " + imgPath + " -A " + outName)
+def convertImgSvgToPdf(imgPath, outName):
+    os.system("inkscape -f " + imgPath + "-A" + outName)
 
 def replaceFigure(figureTag):
     #exchange svgs to pdfs!
-    figureHref = figureTag.find('a').get("href")
+    figureHref = figureTag.get("href")
+    figureHeightFile = figureTag.get("data-file-height") 
+    figureHeightFile = figureTag.get("data-file-width") 
+    figureHeight = figureTag.get("height") 
+    figureHeight = figureTag.get("width") 
     
     fileName =  figureHref[8:]
     filePage = "https://de.wikibooks.org/wiki/Datei:" + fileName
-    downloadPictureFromWikifile(filePage, "Bilder/" + fileName) 
-    convertImgSvgToPdf("Bilder/" + fileName)
+    downloadPictureFromWikifile(filePage, fileName) 
+    convertImgSvgToPdf(fileName, "test1")
 
     #replaceTag(mathTag, newTag)
 
 #Main------------------------------
 if __name__ == "__main__":
 
-    pdb.set_trace()
-
-
-    inputHtmlFileName = argv[1]
-    inputHtmlFile = open(inputHtmlFileName)  
-    inputHtml = inputHtmlFile.read()
-
-    soup = BeautifulSoup(inputHtml , 'html.parser')
-    mathTags = soup.find_all('math')
-    figureTags = soup.find_all('figure')
-
-    for tag in figureTags:
-        replaceFigure(tag)
-    
+     pdb.set_trace()
+#
+    #inputHtmlFileName = argv[1]
+    #inputHtmlFile = open(inputHtmlFileName)  
+    #inputHtml = inputHtmlFile.read()
+#
+    #soup = BeautifulSoup(inputHtml , 'html.parser')
+    #mathTags = soup.find_all('math')
+    #
     #for tag in mathTags:
         #replaceMathTag(tag)
-
-    
-    open(TOPIC + '.tex', 'w').close() #clean file: empty
-    testFile = open(TOPIC + '.tex', 'a')
-    testFile.write(soup.prettify())
-
+#
+    #
+    #open(TOPIC + '.tex', 'w').close() #clean file: empty
+    #testFile = open(TOPIC + '.tex', 'a')
+    #testFile.write(soup.prettify())
+#
 
